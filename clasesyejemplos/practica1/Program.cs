1ccc541a26ead2ef4847fe8d235371f821a4107c
juan <juanosorio1124226@correo.itm.edu.co>
2025-08-24 18:34:14 -0500

    proy

--- clasesyejemplos/practica1/Program.cs
+++ clasesyejemplos/practica1/Program.cs
@@ -1,6 +1,13 @@
 ﻿Console.WriteLine("Hello, World!");
 
-
+Personas persona = new Personas() { id = 1, altura = 1.73m, cedula = 1212323, nombre = "juan" };
+List<Libros> libro = new List<Libros>();
+Libros libro1 = new Libros() { nombre = "el mito de sisifo", editorial = "penguin" };
+libro.Add(libro1);
+foreach (var i in libro)
+{
+    Console.WriteLine(i.nombre, i.editorial);
+}
 public class Personas
 {
     public int id;
@@ -16,5 +23,6 @@ public class Estudiantes : Personas
 
 public class Libros
 {
-
+    public string? nombre;
+    public string? editorial;
 }
