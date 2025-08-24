5655eee1f716fdf9e93d1563598f3cdeebc1fc10
juan <juanosorio1124226@correo.itm.edu.co>
2025-08-24 18:23:03 -0500

    proy

--- clasesyejemplos/practica1/Program.cs
+++ clasesyejemplos/practica1/Program.cs
@@ -1,1 +1,20 @@
 ﻿Console.WriteLine("Hello, World!");
+
+
+public class Personas
+{
+    public int id;
+    public string? nombre;
+    public int cedula;
+    public decimal altura;
+}
+
+public class Estudiantes : Personas
+{
+    public int carnet;
+}
+
+public class Libros
+{
+
+}
